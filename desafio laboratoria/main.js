7b69e9c8d2adaa140c0c861573d341cbb15601e2
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-03-19 10:12:31 -0300

    desafio-laboratoria

---
+++ desafio laboratoria/main.js
@@ -0,0 +1,8 @@
+var name = prompt("Por favor insira seu nome")
+var condition = prompt("Seja bem vindo, " + name +  ". Deseja fazer a prova? 1 ou 2")
+
+if (condition==1){
+    prompt('Em que ano o BTS debutou? \n 1: 2012 \n 2: 2013 \n 3. 2014')
+    prompt("Quantos membros possui o grupo? \n 1. Sete (!!!) \n 2. Cinco \n 3. Treze")
+    prompt("Qual foi o MV de debut do BTS? \n1. We Are Bulletproof Pt2 \n2. N.O. \n3. No More Dream")
+} 
